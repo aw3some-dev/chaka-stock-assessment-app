080524791796319a590af01e421ead1de918f67a
aw3some-dev <olumidesofayo@yahoo.co.uk>
2021-12-04 05:49:53 +0100

    Modified server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 // Serve only the static files form the docs directory
-app.use(express.static(__dirname + '/docs'));
+app.use(express.static(__dirname + "/dist/chaka-stock-market-app"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/docs/index.html'));
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname + "/dist/chaka-stock-market-app/index.html")
+  );
 });
 
 // Start the app by listening on the default Heroku port
